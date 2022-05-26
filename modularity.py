ebbcfe26a4dc82fdf826746769ecbbc3c0c2a7a5
Jadon Blt <jadonbellot@outlook.com>
2022-05-26 09:52:52 +0100

    fubibacci

--- modularity.py
+++ modularity.py
@@ -26,14 +26,43 @@
 
 # func(75,290,1)
 
-x = 10
-y = 20
-def incrementBy(a, b, amount =10):
-    a += amount
-    b += amount
-    return a, b
+# x = 10
+# y = 20
+# def incrementBy(a, b, amount =10):
+#     a += amount
+#     b += amount
+#     return a, b
 
-a ,b = incrementBy(x, y, 15)
-print(a)
-print(b)
+# a ,b = incrementBy(x, y, 15)
+# print(a)
+# print(b)
 
+# def func(*args):
+#     for x in args:
+#         print(x)
+#     print('Done')
+
+# func(1,2,3,4)
+
+# import math # this import
+
+# a = math.sqrt(64)
+# print(a)
+
+# fibonacci series
+
+def fibo(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        if(i == 0):
+            print(a)
+        elif(i == 1):
+            print(b)
+        else:
+            c = a + b
+            print(c)
+            a = b
+            b = c
+
+fibo(7)
